34ec72640b6ffb6f0e0313cf093419245e48827d
Service Web <sw@gssmail.com>
2024-03-13 13:47:30 -0500

    Commit changes in C:\Websites\Git\emarlow\5233\GCG_5233_QA_Inv.sql

---
+++ GCG_5233_QA_Inv.sql
@@ -0,0 +1,8 @@
+/* ==========================================
+ * TABLE: GCG_5233_QA_Inv
+ * ========================================== */
+CREATE TABLE "GCG_5233_QA_Inv"(
+ "Part" CHAR(20) NOT NULL ,
+ "QA_Req" NCHAR(200) NOT NULL );
+
+
