5eccb29d03b32f36eace736d91ff2d0fa7af30ff
Service Web <sw@gssmail.com>
2024-03-13 13:47:30 -0500

    Commit changes in C:\Websites\Git\emarlow\5233\GCG_5233_QA_PO_Ln.sql

---
+++ GCG_5233_QA_PO_Ln.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5233_QA_PO_LN
+ * ========================================== */
+CREATE TABLE "GCG_5233_QA_PO_LN"(
+ "Part" CHAR(20) NOT NULL ,
+ "Purchase_No" CHAR(8) NOT NULL ,
+ "PO_Lines" CHAR(50) NOT NULL ,
+ "QA_Req" NCHAR(200) NOT NULL );
+
+
